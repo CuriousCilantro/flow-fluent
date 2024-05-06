40fc8d89acb46d0093d16c79b8ed1c21b2741f1f
bon-42 <teslaeinstein@Yantra.local>
2024-05-06 14:19:03 -0600

    Updated timer

--- script.js
+++ script.js
@@ -22,11 +22,11 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 
     shortBreakButton.addEventListener('click', function() {
-        setTimer(1 * 60, 'shortBreak'); // 5 minutes
+        setTimer(11 * 60, 'shortBreak'); // 5 minutes
     });
 
     longBreakButton.addEventListener('click', function() {
-        setTimer(11 * 60, 'longBreak'); // 15 minutes
+        setTimer(22 * 60, 'longBreak'); // 15 minutes
     });
 
     resetButton.addEventListener('click', function() {
